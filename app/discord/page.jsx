2fa5899bce351e26a5525d19f2873d4b757126ed
mstfsgdc <iletisim@mstfsgdc.com>
2023-11-24 00:01:46 +0300

    hotfix

--- app/discord/page.jsx
+++ app/discord/page.jsx
@@ -1,12 +1,17 @@
 "use client";
 
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function DiscordPage() {
   const router = useRouter();
 
-  setTimeout(() => {
-    router.push("https://discord.gg/vsTT3ssdks");
-  }, 1);
+  useEffect(() => {
+    const redirectTimeout = setTimeout(() => {
+      router.push("https://discord.gg/vsTT3ssdks");
+    }, 1);
+
+    return () => clearTimeout(redirectTimeout);
+  }, [router]);
   return <div>Discord sunucumuza yönlendiriliyorsunuz...</div>;
 }
