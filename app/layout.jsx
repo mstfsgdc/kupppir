05aefbf58329d0a0da5d6e5bd7911960b31b683c
mstfsgdc <iletisim@mstfsgdc.com>
2023-11-24 01:02:21 +0300

    UPDATE SEO

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,8 +6,29 @@ import Script from "next/script";
 const poppins = Poppins({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"] });
 
 export const metadata = {
-  title: "kupppir",
-  description: "kupppir media",
+  metadataBase: new URL('https://kupppir.vercel.app/'),
+  alternates: {
+    canonical: '/',
+  },
+  title: {
+    template: "%s | kupppir",
+    default: "kupppir",
+  },
+  description: "kupppir'a ait sosyal medya bağlantıları",
+  openGraph: {
+    title: "kupppir",
+    description: "kupppir'a ait sosyal medya bağlantıları",
+    url: "https://www.kupppir.vercel.app/",
+    siteName: "kupppir",
+    images: [
+      {
+        url: "https://kupppir.vercel.app/logo500.png",
+        width: 500,
+        height: 500,
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
